48e5982bbfae70faab114cc2c3b2f8363a2dfa59
GoNEAR <cagatay.morgul@outlook.com>
2025-03-13 10:19:00 +0100

    ajout de style

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 score = 0
-prénom = prompt("Entrez votre prénom")
-let divPrénom = document.querySelector('#prénomEtScore')
-divPrénom.innerHTML = '<h2>'+prénom+'</h2>'+'<h2>'+score+'</h2>'
+do {
+    prénom = prompt("Entrez votre prénom (Entre 3 et 10 lettres)")
+} while ( prénom == null||prénom.length < 3 || prénom.length > 10);
+
+let sectionPrénom = document.querySelector('#prénomEtScore')
+sectionPrénom.innerHTML ='<p>Bienvenue</p>'+'<div><h2>'+prénom+'</h2>'+'<h2 id="score">Ton score: '+score+'</h2></div>'
